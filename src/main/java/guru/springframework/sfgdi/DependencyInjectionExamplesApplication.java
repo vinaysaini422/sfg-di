984278543ac23ee628267472ddbabce9f46d19c0
vinaysaini422@gmail.com <vinaysaini422@gmail.com>
2020-06-23 23:04:25 +0530

    initial commit

---
+++ src/main/java/guru/springframework/sfgdi/DependencyInjectionExamplesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgdi/DependencyInjectionExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
